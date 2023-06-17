28e88e2f84342a3a64aac0cc681629d82dcc4367
Victor Candido <victorjrcandido@gmail.com>
2023-06-16 22:37:21 -0300

    2.1

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,17 +1,31 @@
+import { Negociacoes } from "../models/negociacoes.js";
+
 export class NegociacoesView {
+
+  private element: HTMLElement;
+  
+  constructor(selector: string) {
+    this.element = document.querySelector(selector);
+  }
+
   template(): string {
     return `
     <table class="table table-hover table-bordered">
-      <thead>
-        <tr>
-          <th>DATA</th>
-          <th>QUANTIDADE</th>
-          <th>VALOR</th>
-        </tr>
-      </thead>
-      <tbody>
-      </tbody>
+        <thead>
+            <tr>
+                <th>DATA</th>
+                <th>QUANTIDADE</th>
+                <th>VALOR</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
     </table>
     `
   }
+
+    update(model: Negociacoes): void {
+      this.element.innerHTML = this.template();
+    }
+
 }
--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -1,17 +1,23 @@
 export class NegociacoesView {
+    constructor(selector) {
+        this.element = document.querySelector(selector);
+    }
     template() {
         return `
     <table class="table table-hover table-bordered">
-      <thead>
-        <tr>
-          <th>DATA</th>
-          <th>QUANTIDADE</th>
-          <th>VALOR</th>
-        </tr>
-      </thead>
-      <tbody>
-      </tbody>
+        <thead>
+            <tr>
+                <th>DATA</th>
+                <th>QUANTIDADE</th>
+                <th>VALOR</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
     </table>
     `;
+    }
+    update(model) {
+        this.element.innerHTML = this.template();
     }
 }
